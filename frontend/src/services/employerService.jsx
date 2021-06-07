def60e97f6be75860e06877a62ec36326a903efc
xkendx <63884610+xkendx@users.noreply.github.com>
2021-06-07 16:44:14 +0300

    feat: update components

---
+++ frontend/src/services/employerService.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function employerService() {
+    return <div>Employer Service</div>;
+}
---
+++ frontend/src/services/jobSeekerService.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function jobSeekerService() {
+    return <div>job seeker service</div>;
+}
